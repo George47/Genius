e6e0dbbbfce70a7e46690925d1176c12bce8ed8d
George47 <george.siqi.wang@gmail.com>
2019-07-24 22:37:06 -0400

    simple store layout

--- lib/gen_list.dart
+++ lib/gen_list.dart
@@ -3,9 +3,58 @@ import 'package:flutter/material.dart';
 class GenList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // var deviceSize = MediaQuery.of(context).size;
     return new ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) => Column(),
+        itemCount: 2,
+        itemBuilder: (context, index) => 
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100,
+                color: Colors.red,
+                child: Center(
+                  child: new Text(
+                    "Store",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+              ),
+
+              Flexible(
+                fit: FlexFit.loose,
+                flex: 1,
+                child: new Image.asset(
+                  "assets/images/image1.jpg",
+                  fit: BoxFit.cover,
+                ), 
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+              ),
+              Flexible(
+                fit: FlexFit.loose,
+                flex: 2,
+                child: new Image.asset(
+                  "assets/images/image2.jpg",
+                  fit: BoxFit.cover,
+                ), 
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+              ),
+
+            ],
+          ),
     );
   }
 }
