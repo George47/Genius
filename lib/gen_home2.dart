efd7ebe176193edca7f2d3c7cd32ff4b5cec28ed
George47 <george.siqi.wang@gmail.com>
2019-09-06 23:02:51 -0400

    trying out stack

--- lib/gen_home2.dart
+++ lib/gen_home2.dart
@@ -14,6 +14,38 @@ class _GenHome2State extends State<GenHome2> {
       body: new Center(
         child: new Column(
           children: <Widget>[
+            Stack(
+              alignment: AlignmentDirectional.bottomCenter,
+              children: <Widget>[
+                SizedBox(
+                  width: 120,
+                  height: 120,
+                  child: const DecoratedBox(
+                    decoration: const BoxDecoration(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 90,
+                  height: 90,
+                  child: const DecoratedBox(
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: const DecoratedBox(
+                    decoration: const BoxDecoration(
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+              ],
+            ),
             Text('TEST2'),
             IconButton(icon: new Icon(Icons.pages),
               onPressed: () { 
